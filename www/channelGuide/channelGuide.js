1308894de21d0e23379503fb0811d9d8737a2659
Ted Shaffer <ted@brightsign.biz>
2016-03-08 12:38:23 -0800

    spinner icon while fetching channel guide data

--- www/channelGuide/channelGuide.js
+++ www/channelGuide/channelGuide.js
@@ -499,9 +499,14 @@ angular.module('jtr.controllers')
   $scope.numDaysEpgData = 3;
   //$scope.retrieveEpgData();
 
+  $scope.spinnerClass = "centerIcon";
+  $scope.fetchingData = true;
+
   var promise = jtrEpgFactory.retrieveEpgData();
   promise.then(function() {
     console.log("jtrEpgFactory returned success");
+    $scope.spinnerClass = "dontCenterIcon";
+    $scope.fetchingData = false;
 
     $scope.epgProgramSchedule = jtrEpgFactory.getEpgProgramSchedule();
     $scope.epgProgramScheduleStartDateTime = jtrEpgFactory.getEpgProgramScheduleStartDateTime();
